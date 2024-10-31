7cc3ccfb175d7883ec1224e409d96689800d1f06
sung-silver <jinna0319@gmail.com>
2024-10-31 14:00:15 +0900

    [DEL] 커스텀 요청 dto 삭제

--- fcm/src/main/java/com/practice/fcm/external/fcm/FCMMessage.java
+++
@@ -1,7 +0,0 @@
-package com.practice.fcm.external.fcm;
-
-public record FCMMessage(boolean validateOnly, Message message) {
-    public static FCMMessage of(boolean validateOnly, Message message) {
-        return new FCMMessage(validateOnly, message);
-    }
-}
